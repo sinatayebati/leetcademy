0bd96603979267d0c4172307e50677d6a4536150
sinatayebati <sinatayebati@gmail.com>
2024-02-25 14:13:34 -0600

    #21 top bar for big media queries updated

--- src/components/common/Topbar/Index.jsx
+++ src/components/common/Topbar/Index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 import './Index.scss'
 import LeetCademyLogo from '../../../assets/LeetCademy-logos/LeetCademy-logos_black.png';
+import user from '../../../assets/user2.png'
 import { RiHome3Line } from "react-icons/ri";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
 import { RiBriefcaseLine } from "react-icons/ri";
@@ -27,12 +28,45 @@ export default function Topbar() {
     <div className='topbar-main'>
       <img src={LeetCademyLogo} alt="LeetCademyLogo" className="LeetCademyLogo-topbar" />
       <div className='react-icons'>
+        <div className="icon-container">
+          <IoMdSearch className='react-icon'/>
+          <span className="icon-label">Search</span>
+        </div>
+        <div className="icon-container">
+          <RiHome3Line className='react-icon'/>
+          <span className="icon-label">Home</span>
+        </div>
+        <div className="icon-container">
+          <AiOutlineUsergroupAdd className='react-icon'/>
+          <span className="icon-label">My Network</span>
+        </div>
+        <div className="icon-container">
+          <RiBriefcaseLine className='react-icon'/>
+          <span className="icon-label">Jobs</span>
+        </div>
+        <div className="icon-container">
+          <BiMessageSquareDetail className='react-icon'/>
+          <span className="icon-label">Messaging</span>
+        </div>
+        <div className="icon-container">
+          <AiOutlineNotification className='react-icon'/>
+          <span className="icon-label">Notifications</span>
+        </div>
+        <div className="icon-container">
+          <img src={user} alt="user" className="user" />
+          <span className="icon-label">Profile</span>
+        </div>
+
+        {/*
         <ResponsiveIcon><IoMdSearch className='react-icon'/></ResponsiveIcon>
         <ResponsiveIcon><RiHome3Line className='react-icon'/></ResponsiveIcon>
         <ResponsiveIcon><AiOutlineUsergroupAdd className='react-icon'/></ResponsiveIcon>
         <ResponsiveIcon><RiBriefcaseLine className='react-icon'/></ResponsiveIcon>
         <ResponsiveIcon><BiMessageSquareDetail className='react-icon'/></ResponsiveIcon>
         <ResponsiveIcon><AiOutlineNotification className='react-icon'/></ResponsiveIcon>
+        <img src={user} alt="user" className="user" />
+        */}
+    
       </div>
     </div>
   )
